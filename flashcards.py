aff2aceb4d3f7be6fbaee015bd23be4465c5cd8f
Michael Broniola <mbroniola@gmail.com>
2020-06-21 08:48:53 -0700

    Initial commit

---
+++ flashcards.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    return render_template("welcome.html", message="Here's a message from the view.")
+
+
